48dd98d9f0b3195f0dd1cc8c99776432db889203
jiaen188 <244189981@qq.com>
2018-09-06 20:36:37 +0800

    使用chai-spies  监听 事件

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ new Vue({
 
 // 单元测试
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+
 const expect = chai.expect
 
 {
@@ -96,10 +99,11 @@ const expect = chai.expect
         }
     })
     vm.$mount()
-    vm.$on('click', function() {
-        console.log(1)
-        // 希望这个函数被执行
-    })
+    let spy = chai.spy(function() {})
+
+    // 希望这个函数被执行
+    vm.$on('click', spy)
     const button = vm.$el
     button.click()
+    expect(spy).to.have.been.called()
 }
