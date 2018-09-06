03baefb9fde4f9bedc6b99f59b6f4c7ec860b696
jiaen188 <244189981@qq.com>
2018-09-06 17:44:14 +0800

    检测 g-button-group 的子元素，是否是 button

--- src/button-group.vue
+++ src/button-group.vue
@@ -3,7 +3,19 @@
         <slot></slot> 
     </div>
 </template>
-<script></script>
+<script>
+export default {
+    mounted() {
+        console.log(this.$el.children);
+        for (const node of this.$el.children) {
+            const name = node.nodeName.toLowerCase()
+            if (name !== 'button') {
+                console.warn(`g-button-group的子元素应该全是 g-button，但是你写的是 ${name}`)
+            }
+        }
+    }
+}
+</script>
 <style lang="scss">
     .g-button-group {
         display: inline-flex;
