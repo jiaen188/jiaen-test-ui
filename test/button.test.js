cad4fe9aa57fdab75398fe3948eeffbf8aea802a
jiaen188 <244189981@qq.com>
2018-09-09 22:26:23 +0800

    使用Mocha & Chai做单元测试

--- test/button.test.js
+++ test/button.test.js
@@ -5,10 +5,13 @@ import Button from '../src/button'
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
+// BDD 行为驱动测试
 describe('Button', () => {
+
     it('存在.', () => {
         expect(Button).to.be.ok
     })
+
     it('可以设置icon.', () => {
         const Constructor = Vue.extend(Button)
         const vm = new Constructor({
@@ -20,6 +23,7 @@ describe('Button', () => {
         expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
         vm.$destroy()
     })
+
     it('可以设置loading.', () => {
         const Constructor = Vue.extend(Button)
         const vm = new Constructor({
@@ -33,6 +37,7 @@ describe('Button', () => {
         expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
         vm.$destroy()
     })
+
     it('icon 默认的 order 是 1', () => {
         const div = document.createElement('div')
         document.body.appendChild(div)
@@ -47,6 +52,7 @@ describe('Button', () => {
         vm.$el.remove()
         vm.$destroy()
     })
+
     it('设置 iconPosition 可以改变 order', () => {
         const div = document.createElement('div')
         document.body.appendChild(div)
@@ -62,6 +68,7 @@ describe('Button', () => {
         vm.$el.remove()
         vm.$destroy()
     })
+
     it('点击 button 触发 click 事件', () => {
         const Constructor = Vue.extend(Button)
         const vm = new Constructor({
