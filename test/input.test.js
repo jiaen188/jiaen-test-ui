0abf35490c321c6060eb83cc270f9ad9653c0e88
jiaen188 <244189981@qq.com>
2018-09-12 15:51:39 +0800

    使用beforeEach和afterEach复用测试用例

--- test/input.test.js
+++ test/input.test.js
@@ -13,57 +13,57 @@ describe('Button', () => {
     })
 
     describe('input props', () => {
+
+      const Constructor = Vue.extend(Input)
+
+      let vm
+      afterEach(() => {
+        vm.$destroy()
+      })
+
       it('接收 value', () => {
-        const Constructor = Vue.extend(Input)
-        const vm = new Constructor({
-        propsData: {
-            value: '1234'
-        }
+        vm = new Constructor({
+          propsData: {
+              value: '1234'
+          }
         }).$mount()
         const inputElement = vm.$el.querySelector('input')
         expect(inputElement.value).to.equal('1234')
-        vm.$destroy()
       })
   
       it('接收 disabled', () => {
-        const Constructor = Vue.extend(Input)
-        const vm = new Constructor({
-        propsData: {
-            disabled: true
-        }
+        vm = new Constructor({
+          propsData: {
+              disabled: true
+          }
         }).$mount()
         const inputElement = vm.$el.querySelector('input')
         expect(inputElement.disabled).to.equal(true)
-        vm.$destroy()
       })
   
       // 注意 readOnly  大小写区分，不然测试用例通不过
       it('接收 readonly', () => {
-        const Constructor = Vue.extend(Input)
-        const vm = new Constructor({
-        propsData: {
-          readonly: true
-        }
+        vm = new Constructor({
+          propsData: {
+            readonly: true
+          }
         }).$mount()
         const inputElement = vm.$el.querySelector('input')
         console.log(inputElement.outerHTML)
         expect(inputElement.readOnly).to.equal(true)
-        vm.$destroy()
       })
   
       it('接收 error', () => {
-        const Constructor = Vue.extend(Input)
-        const vm = new Constructor({
-        propsData: {
-          error: '输入出错了'
-        }
+        vm = new Constructor({
+          propsData: {
+            error: '输入出错了'
+          }
         }).$mount()
         const useElement = vm.$el.querySelector('use')
         expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
   
         const errorMessage = vm.$el.querySelector('.error-message')
         expect(errorMessage.innerText).to.equal('输入出错了')
-        vm.$destroy()
       })
 
     })
