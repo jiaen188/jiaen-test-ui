07ae1a2d2670dee8a9003ecebbfa33a599eabb82
jiaen188 <244189981@qq.com>
2018-09-13 15:19:19 +0800

    scss的for 实现col-类名的样式

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './input'
+import Row from './Row'
+import Col from './Col'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
     el: '#app',
---
+++ src/col.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        span: {
+            type: [Number, String]
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.col {
+    height: 100px;
+    width: 50%;
+    background: grey;
+    border: 1px solid red;
+
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+            width: ($n / 24) * 100%;
+        }
+    }
+}
+</style>
