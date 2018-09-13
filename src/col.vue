a3c7a27680dce605e676778897c407b4479cd406
jiaen188 <244189981@qq.com>
2018-09-13 20:55:33 +0800

    实现gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+        :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+        <div style="border: 1px solid green; height: 100px;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
@@ -9,7 +12,21 @@ export default {
     props: {
         span: {
             type: [Number, String]
+        },
+        offset: {
+            type: [Number, String]
         }
+    },
+    data () {
+        return {
+            gutter: 0
+        }
+    },
+    created () {
+        console.log('col created')
+    },
+    mounted () {
+        console.log('col mounted')
     }
 }
 </script>
@@ -19,13 +36,21 @@ export default {
 .col {
     height: 100px;
     width: 50%;
-    background: grey;
-    border: 1px solid red;
+    // padding: 0 10px;
+    // background: grey;
+    // border: 1px solid red;
 
     $class-prefix: col-;
     @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
             width: ($n / 24) * 100%;
+        }
+    }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+            margin-left: ($n / 24) * 100%;
         }
     }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,25 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        gutter: [Number, String]
+    },
+    created () {
+        console.log('row created')
+    },
+    mounted () {
+        console.log('row mounted')
+        this.$children.forEach(vm => {
+            vm.gutter = this.gutter
+        })
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 .row {
