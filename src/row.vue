56b66c7bcdfcc8e2fcca02eace0d02793269f714
jiaen188 <244189981@qq.com>
2018-09-14 14:12:37 +0800

    添加row的align

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="rowStyle">
+    <div class="row" :style="rowStyle" :class="rowClass">
         <slot></slot>
     </div>
 </template>
@@ -7,9 +7,21 @@
 <script>
 export default {
     props: {
-        gutter: [Number, String]
+        gutter: {
+            type: [Number, String]
+        },
+        align: {
+            type: String,
+            validator (value) {
+                return ['left', 'right', 'center'].includes(value)
+            }
+        }
     },
     computed: {
+        rowClass () {
+            let { align } = this
+            return [align &&  `align-${align}`]
+        },
         rowStyle () {
             let { gutter } = this
             return {
@@ -29,5 +41,14 @@ export default {
 <style lang="scss" scoped>
 .row {
     display: flex;
+    &.align-left {
+        justify-content: flex-start;
+    }
+    &.align-right {
+        justify-content: flex-end;
+    }
+    &.align-center {
+        justify-content: center;
+    }
 }
 </style>
