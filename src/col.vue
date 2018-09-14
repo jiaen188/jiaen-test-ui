94873cd5289fb79e78614712959bb339174867b6
jiaen188 <244189981@qq.com>
2018-09-14 13:48:15 +0800

    重构row  col

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-        :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-        <div style="border: 1px solid green; height: 100px;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass" :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 
@@ -22,11 +19,20 @@ export default {
             gutter: 0
         }
     },
-    created () {
-        console.log('col created')
-    },
-    mounted () {
-        console.log('col mounted')
+    computed: {
+        colClass () {
+            let { span, offset } = this
+            return [
+                span && `col-${span}`, 
+                offset && `offset-${offset}`
+            ]
+        },
+        colStyle () {
+            return {
+                paddingLeft: this.gutter / 2 + 'px', 
+                paddingRight: this.gutter / 2 + 'px'
+            }
+        }
     }
 }
 </script>
