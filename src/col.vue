7dfe4269c220299aedd65a4d7769a6f96addd6a5
jiaen188 <244189981@qq.com>
2018-09-17 21:03:24 +0800

    重构col.vue的部分内容

--- src/col.vue
+++ src/col.vue
@@ -34,17 +34,25 @@ export default {
             gutter: 0
         }
     },
+    methods: {
+        createClasses (obj, str = '') {
+            if (!obj) {return []}
+            let array = []
+            if (obj.span) { array.push(`col-${str}${obj.span}`) }
+            if (obj.offset) { array.push(`offset-${str}${obj.offset}`) }
+            return array
+        }
+    },
     computed: {
         colClass () {
             let { span, offset, phone, ipad, narrowPc, pc, widePc } = this
-            let phoneClass = []
+            let createClasses = this.createClasses
             return [
-                span && `col-${span}`, 
-                offset && `offset-${offset}`,
-                ...(ipad ? [`col-ipad-${ipad.span}`] : []),
-                ...(narrowPc ? [`col-narrow-${narrowPc.span}`] : []),
-                ...(pc ? [`col-pc-${pc.span}`] : []),
-                ...(widePc ? [`col-wid-pc-${widePc.span}`] : [])
+                ...createClasses({span, offset}),
+                ...createClasses(ipad, 'ipad-'),
+                ...createClasses(narrowPc, 'narrowPc-'),
+                ...createClasses(pc, 'pc-'),
+                ...createClasses(widePc, 'widePc-')
             ]
         },
         colStyle () {
