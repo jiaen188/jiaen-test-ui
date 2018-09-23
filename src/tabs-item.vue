023358db11f4f980c413d21ace63cf655da2db28
jiaen188 <244189981@qq.com>
2018-09-24 01:16:40 +0800

    tabs 支持 disabled

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item" @click="xxx" :class="classes">
+    <div class="tabs-item" @click="onClick" :class="classes">
         <slot></slot>
     </div>
 </template>
@@ -26,7 +26,8 @@ export default {
     computed: {
         classes () {
             return {
-                active: this.active
+                active: this.active,
+                disabled: this.disabled
             }
         }
     },
@@ -36,7 +37,8 @@ export default {
         })
     },
     methods: {
-        xxx () {
+        onClick () {
+            if (this.disabled) { return }
             this.eventBus.$emit('update:selected', this.name, this)
         }
     }
@@ -45,6 +47,7 @@ export default {
 
 <style lang="scss" scoped>
 $blue: blue;
+$disabled-text-color: grey;
 .tabs-item {
     flex-shrink: 0;
     padding: 0 2em;
@@ -55,6 +58,9 @@ $blue: blue;
     &.active {
         color: $blue;
         font-weight: blod;
+    }
+    &.disabled {
+        color: $disabled-text-color;
     }
 }
 </style>
