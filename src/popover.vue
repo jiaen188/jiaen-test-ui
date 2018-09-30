cad63a2fb6c738af0f368be23abaf4da134b9590
jiaen188 <244189981@qq.com>
2018-09-30 10:34:26 +0800

    减小popover尖角的大小,清除抖动

--- src/popover.vue
+++ src/popover.vue
@@ -161,10 +161,12 @@ $border-radius: 4px;
         }
         &::before {
             border-top-color: black;
+            border-bottom: none;
             top: 100%;
         }
         &::after {
             border-top-color: white;
+            border-bottom: none;
             top: calc(100% - 1px);
         }
     }
@@ -174,10 +176,12 @@ $border-radius: 4px;
             left: 10px;
         }
         &::before {
+            border-top: none;
             border-bottom-color: black;
             bottom: 100%;
         }
         &::after {
+            border-top: none;
             border-bottom-color: white;
             bottom: calc(100% - 1px);
         }
@@ -191,10 +195,12 @@ $border-radius: 4px;
         }
         &::before {
             border-left-color: black;
+            border-right: none;
             left: 100%;
         }
         &::after {
             border-left-color: white;
+            border-right: none;
             left: calc(100% - 1px);
         }
     }
@@ -206,10 +212,12 @@ $border-radius: 4px;
         }
         &::before {
             border-right-color: black;
+            border-left: none;
             right: 100%;
         }
         &::after {
             border-right-color: white;
+            border-left: none;
             right: calc(100% - 1px);
         }
     }
