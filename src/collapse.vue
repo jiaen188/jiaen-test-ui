bb86a881575a0dbad416ab303af9b0e0dc497ed4
jiaen188 <244189981@qq.com>
2018-10-01 22:08:06 +0800

    添加 collapse的单个展开选项

--- src/collapse.vue
+++ src/collapse.vue
@@ -5,8 +5,26 @@
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
-    
+    props: {
+        single: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data () {
+        return {
+            eventBus: new Vue()
+        }
+    },
+    provide () {
+        if (this.single) {
+            return {
+                eventBus: this.eventBus
+            }
+        }
+    }
 }
 </script>
 
