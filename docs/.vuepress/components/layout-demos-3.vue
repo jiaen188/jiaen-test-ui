fb5918e221d222931da0ec5f71d0092211917346
jiaen188 <244189981@qq.com>
2018-10-05 15:35:24 +0800

    增加 layout 预览地址

---
+++ docs/.vuepress/components/layout-demos-3.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <h2>布局</h2>
+        <p><strong>预览</strong></p>
+
+        <g-layout style="color: white; overflow:hidden; margin-bottom:50px;">
+            <g-sider style=" background:#ddd; width:200px; color: black;">
+                sider
+            </g-sider>
+            <g-layout>
+                <g-header style="height: 50px; background:lightskyblue;">
+                    header
+                </g-header>
+                <g-content style="height: 100px; background:deepskyblue;">
+                    content
+                </g-content>
+                <g-footer style="height: 50px; background:lightskyblue;">
+                    footer
+                </g-footer>
+            </g-layout>
+        </g-layout>
+
+        <p><strong>代码</strong></p>
+        <pre><code>{{content}}</code></pre>
+    </div>
+</template>
+
+<script>
+import layout from '../../../src/layout.vue' 
+import header from '../../../src/header.vue' 
+import content from '../../../src/content.vue' 
+import footer from '../../../src/footer.vue' 
+import sider from '../../../src/sider.vue' 
+
+export default {
+    components: {
+        'g-layout': layout,
+        'g-header': header,
+        'g-content': content,
+        'g-footer': footer,
+        'g-sider': sider
+    },
+    data () {
+        return {
+            content: `
+            <g-layout style="color: white; overflow:hidden; margin-bottom:50px;">
+                <g-sider style=" background:#ddd; width:200px; color: black;">
+                    sider
+                </g-sider>
+                <g-layout>
+                    <g-header style="height: 50px; background:lightskyblue;">
+                        header
+                    </g-header>
+                    <g-content style="height: 100px; background:deepskyblue;">
+                        content
+                    </g-content>
+                    <g-footer style="height: 50px; background:lightskyblue;">
+                        footer
+                    </g-footer>
+                </g-layout>
+            </g-layout>
+            `.replace(/^ {8}/gm, '').trim()
+        }
+    }
+}
+</script>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+}
+</style>
--- docs/components/layout.md
+++ docs/components/layout.md
@@ -3,3 +3,9 @@ title: Layout - 布局
 ---
 
 # 布局
+
+<ClientOnly>
+  <layout-demos-1></layout-demos-1>
+  <layout-demos-2></layout-demos-2>
+  <layout-demos-3></layout-demos-3>
+</ClientOnly>
